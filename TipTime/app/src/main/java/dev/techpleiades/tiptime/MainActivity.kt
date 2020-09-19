54f1436694da655de40a0cfcb9d3fa0b701aef2f
Mitsuhiro Komuro <mitsuhiro.komuro@gmail.com>
2020-09-19 16:23:56 +0900

    Add view binding

--- TipTime/app/src/main/java/dev/techpleiades/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/dev/techpleiades/tiptime/MainActivity.kt
@@ -2,10 +2,15 @@ package dev.techpleiades.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.techpleiades.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
