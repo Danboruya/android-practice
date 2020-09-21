fe13d211b05ee238af454c2150344d3834b54e8f
Mitsuhiro Komuro <mitsuhiro.komuro@gmail.com>
2020-09-21 10:55:02 +0900

    Add RecyclerView in Affirmations app

--- Affirmations/app/src/main/java/dev/techpleiades/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/dev/techpleiades/affirmations/MainActivity.kt
@@ -2,10 +2,22 @@ package dev.techpleiades.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import dev.techpleiades.affirmations.adapter.ItemAdapter
+import dev.techpleiades.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize data
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Improve performance setting
+        recyclerView.setHasFixedSize(true)
     }
 }
---
+++ Affirmations/app/src/main/java/dev/techpleiades/affirmations/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package dev.techpleiades.affirmations.model
+
+data class Affirmation (val stringResourceId: Int)
