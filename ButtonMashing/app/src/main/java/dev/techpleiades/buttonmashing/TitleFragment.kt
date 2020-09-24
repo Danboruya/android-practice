5f9e0bac103650604ca4496003acd2afe2085078
Mitsuhiro Komuro <mitsuhiro.komuro@gmail.com>
2020-09-24 21:09:03 +0900

    Add onClickListener to title fragment for ButtonMashing app

--- ButtonMashing/app/src/main/java/dev/techpleiades/buttonmashing/TitleFragment.kt
+++ ButtonMashing/app/src/main/java/dev/techpleiades/buttonmashing/TitleFragment.kt
@@ -1,10 +1,12 @@
 package dev.techpleiades.buttonmashing
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 /**
  * A simple [Fragment] subclass.
@@ -19,5 +21,12 @@ class TitleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_title, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.start_button).setOnClickListener {
+            Log.d("click", "Button is clicked!")
+        }
     }
 }
